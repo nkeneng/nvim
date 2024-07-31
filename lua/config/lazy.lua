f045b556328a80ec5d39828276ffdcadb75230eb
steven nkeneng <nkeneng.junior@yahoo.fr>
2024-07-31 07:03:08 +0000

    added more extra plugins for js

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -15,11 +15,12 @@ require("lazy").setup({
 		opts = { colorscheme = "xcode" }
 	},
     -- import any extras modules here
-    -- { import = "lazyvim.plugins.extras.lang.typescript" },
-    -- { import = "lazyvim.plugins.extras.lang.json" },
-    -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
-    -- import/override with your plugins
+    { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.coding.copilot" },
+    { import = "lazyvim.plugins.extras.formatting.prettier" },
+    { import = "lazyvim.plugins.extras.lang.typescript" },
+    { import = "lazyvim.plugins.extras.lang.json" },
+    -- import/override with your plugins
     { import = "plugins" },
     -- { import = "plugins/telescope" },
     -- { import = "plugins/php" },
