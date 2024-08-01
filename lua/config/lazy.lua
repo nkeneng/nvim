70fb673b6247b791322aacd13034d36be9d0652e
stevennkeneng <nkeneng.junior@yahoo.fr>
2024-08-01 15:37:20 +0200

    fixed phpActor not working

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -5,15 +5,14 @@ if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
 end
 vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
-
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     {
-		"LazyVim/LazyVim",
-		import = "lazyvim.plugins",
-		opts = { colorscheme = "xcode" }
-	},
+      "LazyVim/LazyVim",
+      import = "lazyvim.plugins",
+      opts = { colorscheme = "xcode" },
+    },
     -- import any extras modules here
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.coding.copilot" },
@@ -25,8 +24,8 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.markdown" },
     -- import/override with your plugins
     { import = "plugins" },
-    -- { import = "plugins/telescope" },
-    -- { import = "plugins/php" },
+    { import = "plugins/telescope" },
+    { import = "plugins/php" },
     -- { import = "plugins/42" },
   },
   defaults = {
--- lua/plugins/php/phpactor.lua
+++ lua/plugins/php/phpactor.lua
@@ -1,28 +1,47 @@
 return {
-	{
-		"gbprod/phpactor.nvim",
-		ft = 'php',
-		build = function()
-			require("phpactor.handler.update")()
-		end,
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"neovim/nvim-lspconfig"
-		},
-		config = function()
-			vim.keymap.set('n', '<Leader>pA', ':PhpActor<CR>')
-		end,
-	},
-	{
-		"phpactor/phpactor",
-		lazy = false,
-		ft = 'php',
-		run = 'composer install --no-dev --optimize-autoloader',
-		config = function()
-			vim.keymap.set('n', '<Leader>pm', ':PhpactorContextMenu<CR>')
-			vim.keymap.set('n', '<Leader>pn', ':PhpactorClassNew<CR>')
-			vim.keymap.set('n', '<Leader>gd', ':PhpactorGotoDefinition<CR>')
-			vim.keymap.set('n', '<Leader>em', ':PhpactorExtractMethod<CR>')
-		end,
-	}
+  {
+    "gbprod/phpactor.nvim",
+    ft = "php",
+    build = function()
+      require("phpactor.handler.update")()
+    end,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "neovim/nvim-lspconfig",
+    },
+    opts = {
+      install = {
+        path = vim.fn.stdpath("data") .. "/lazy/phpactor",
+        branch = "master",
+        bin = vim.fn.stdpath("data") .. "/lazy/phpactor/bin/phpactor",
+        php_bin = "php",
+        composer_bin = "composer",
+        git_bin = "git",
+        check_on_startup = "none",
+      },
+      lspconfig = {
+        enabled = true,
+        options = {},
+      },
+    },
+    keys = {
+      {
+        "<leader>pA",
+		":PhpActor<CR>",
+        desc = "PhpActor",
+      },
+    },
+  },
+  {
+    "phpactor/phpactor",
+    lazy = false,
+    ft = "php",
+    run = "composer install --no-dev --optimize-autoloader",
+    config = function()
+      vim.keymap.set("n", "<Leader>pm", ":PhpactorContextMenu<CR>")
+      vim.keymap.set("n", "<Leader>pn", ":PhpactorClassNew<CR>")
+      vim.keymap.set("n", "<Leader>gd", ":PhpactorGotoDefinition<CR>")
+      vim.keymap.set("n", "<Leader>em", ":PhpactorExtractMethod<CR>")
+    end,
+  },
 }
