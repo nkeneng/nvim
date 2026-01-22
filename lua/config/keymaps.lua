12ab689fc0812e294c32d5d3a0da8e33a9f078be
Steven Nkeneng <nkeneng.junior@yahoo.fr>
2026-01-22 18:56:56 +0100

    reset lazyvim to fix issues

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,8 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
--- In your init.lua (for Lua configuration)
---
 -- vim.keymap.set("n", "<leader>ig", ":lua require('telescope').extensions.live_grep_args.live_grep_args({ search_dirs = {vim.fn.getcwd()} })<CR>")
 local keymap = vim.keymap
 local opts = { noremap = true, silent = true }
